ef63a54224fcfd3982c1ca2840d2d9b01c1f109b
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-07 10:21:45 +0100

    post request

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const express = require('express')
-const  app = express()
+const  Joi = require('joi');
+const express = require('express');
+const  app = express();
+
+app.use(express.json());
+
 const courses = [
     {id: 1, name:'edu101'},
     {id: 2, name:'stp101'},
@@ -41,6 +45,19 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 
 });
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+})
+
+
+
+
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
